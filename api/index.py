1386226d4f5c8cab2d8ef9436d7176709fb038ac
Levii <137891994+Leviiistaken@users.noreply.github.com>
2025-04-27 14:47:36 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,21 +1,22 @@
-import os
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
-WEBHOOK_URL = os.getenv("SECRET_KEY")
+DISCORD_WEBHOOK_URL = os.environ.get('DISCORD_WEBHOOK_URL')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-@app.route("/", methods=["POST"])
+@app.route('/', methods=['POST'])
 def proxy_webhook():
-    if not WEBHOOK_URL:
-        return "Webhook URL not set.", 500
+    key = request.headers.get('Authorization')
+    if key != SECRET_KEY:
+        return 'Unauthorized', 401
 
     data = request.get_json()
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(WEBHOOK_URL, json=data, headers=headers)
 
-    return "OK", response.status_code
+    response = requests.post(DISCORD_WEBHOOK_URL, json=data)
+    return 'OK', response.status_code
 
 if __name__ == "__main__":
     app.run()
